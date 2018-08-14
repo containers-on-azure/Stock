e51d7f8f50bc1f80bb04607f4d80cc91694f3a99
Francisco Beltrao <francisco.beltrao@outlook.com>
2018-08-14 10:55:35 +0200

    Version 0 of Stock Api (#1)
    Get all and specific symbol

---
+++ src/Stock.Api/Models/StockViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Api.Models
+{
+    public class StockViewModel
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public decimal TodaysHigh { get; set; }
+        public decimal TodaysLow { get; set; }
+        public string Name { get; set; }
+    }
+}
